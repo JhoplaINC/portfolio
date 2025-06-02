1cf339b1b2ccf10964c60a95d815c2ebed8c58ca
Jhopla <jorge.plaza.gj@gmail.com>
2025-06-02 11:28:37 -0400

    next config updated

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
-    basePath: "/portfolio",
-    assetPrefix: "/portfolio/",
+    basePath: "/",
+    assetPrefix: "/",
 };
 
 export default nextConfig;
